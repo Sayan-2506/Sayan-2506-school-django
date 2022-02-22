2b91acfbff8e8643358bd112793bc91119d0e1f8
mrrobotxrp <61610731+Sayan-2506@users.noreply.github.com>
2022-02-22 11:34:23 +0600

    Add files via upload

---
+++ mainapp/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from .models import *
+
+
+
+admin.site.register(New)
+admin.site.register(Choice)
+admin.site.register(Comment)
+admin.site.register(Course)
+admin.site.register(CourseDetail)
+admin.site.register(Teacher)
